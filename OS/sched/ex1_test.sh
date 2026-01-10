7ba529a5483adbc7b7eb357627cca583589b6c02
itaysam100 <itaysamuelov@campus.technio.ac.il>
2026-01-11 01:54:48 +0200

    insert code for ex1_test.sh

---
+++ OS/sched/ex1_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for i in {1..3}; do
+  ./ex1 > out.txt
+  cat out.txt
+  cat out.txt | grep "exit"
+done
+rm -f out.txt
+
+echo "3 runs completed"
+
+FILE = "ex1.cpp"
+OPT0 = "const int nice_vals[3] = {0, 10, 19};"
+OPT1 = "const int nice_vals[3] = {0, 0, 0};"
+OPT2 = "const int nice_vals[3] = {0, 5, 19};"
+OPT3 = "const int nice_vals[3] = {0, 15, 19};"
+
+for i in {1..3} ; do
+  's/OPT$((i-1))/OPT$i/g' FILE
+  make clean
+  make
+  ./ex1
+done
+
