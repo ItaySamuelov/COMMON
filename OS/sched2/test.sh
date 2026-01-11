0dd939081decf9366a5f9d322dea9e7ad9b0217c
itaysam100 <itaysamuelov@campus.technio.ac.il>
2026-01-11 12:57:20 +0200

    create test.sh and makefile content, undebugged

---
+++ OS/sched2/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+make clean
+make
+for i in {1..3}; do
+  echo -e "\n"
+  echo "run number $((i))"
+  ./ex1
+done
+rm -f out.txt
+
+echo "3 runs completed"
+
+FILE="ex1.cpp"
+OPT0="{0, 10, 19};"
+OPT1="{0, 0, 0};"
+OPT2="{0, 5, 19};"
+OPT3="{0, 15, 19};"
+
+for i in {1..3} ; do
+  echo -e "\n"
+  echo "option $((i))"
+  PREV="OPT$(($i-1))"
+  NEXT="OPT$i"
+  sed -i "s/${!PREV}/${!NEXT}/g" "$FILE"
+  make clean
+  make
+  ./ex1
+done
+
+sed -i "s/${!NEXT}/$OPT0/g" "$FILE" #restore to default
+make clean
+make
