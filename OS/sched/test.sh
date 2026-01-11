3e1cfb7989cf6374ea6e1dae9b1c9084c2978736
itaysam100 <itaysamuelov@campus.technio.ac.il>
2026-01-11 02:43:46 +0200

    test file fixed

--- OS/sched/test.sh
+++ OS/sched/test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+make clean
+make
 for i in {1..3}; do
-  ex1 > out.txt
+  ./ex1 > out.txt
+  echo -e "\n"
+  echo "run number $((i))"
   cat out.txt
   cat out.txt | grep "exit"
 done
@@ -8,14 +12,14 @@ rm -f out.txt
 
 echo "3 runs completed"
 
-FILE = "ex1.cpp"
-OPT0 = "const int nice_vals[3] = {0, 10, 19};"
-OPT1 = "const int nice_vals[3] = {0, 0, 0};"
-OPT2 = "const int nice_vals[3] = {0, 5, 19};"
-OPT3 = "const int nice_vals[3] = {0, 15, 19};"
+FILE="ex1.cpp"
+OPT0="const int nice_vals[3] = {0, 10, 19};"
+OPT1="const int nice_vals[3] = {0, 0, 0};"
+OPT2="const int nice_vals[3] = {0, 5, 19};"
+OPT3="const int nice_vals[3] = {0, 15, 19};"
 
 for i in {1..3} ; do
-  eho "\n"
+  echo -e "\n"
   echo "option $((i))"
   PREV = OPT$((i-1))
   NEXT = OPT$i
