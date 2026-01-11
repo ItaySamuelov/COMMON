002525fca24cdf8bf7fe4d98928dbef52e73f6c5
itaysam100 <itaysamuelov@campus.technio.ac.il>
2026-01-11 04:46:00 +0200

    finaly, it works! the trick was using

--- OS/sched/test.sh
+++ OS/sched/test.sh
@@ -11,17 +11,18 @@ rm -f out.txt
 echo "3 runs completed"
 
 FILE="ex1.cpp"
-OPT0="const int nice_vals[3] = {0, 10, 19};"
-OPT1="const int nice_vals[3] = {0, 0, 0};"
-OPT2="const int nice_vals[3] = {0, 5, 19};"
-OPT3="const int nice_vals[3] = {0, 15, 19};"
+OPT0="{0, 10, 19};"
+OPT1="{0, 0, 0};"
+OPT2="{0, 5, 19};"
+OPT3="{0, 15, 19};"
 
 for i in {1..3} ; do
   echo -e "\n"
   echo "option $((i))"
   PREV="OPT$(($i-1))"
   NEXT="OPT$i"
-  sed -i 's/$PREV/$NEXT/g' "$FILE"
+
+  sed -i "s/${!PREV}/${!NEXT}/g" "$FILE"
   make clean
   make
   ./ex1
